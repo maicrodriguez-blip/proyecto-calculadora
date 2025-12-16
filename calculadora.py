ef5a086151bbccfdfa30d45fd254aef327ed7bdf
maicrodriguez-blip <maicrodriguez@est.ecotec.edu.ec>
2025-12-16 00:12:08 -0500

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+class Calculadora:
+  def suma(self, a, b):
+    return a + b
+  def resta(self, a, b):
+    return a - b
+  def divide(self, a, b):
+    if b == 0:
+      raise ValueError("División por cero no permitida")
+    return a / b
